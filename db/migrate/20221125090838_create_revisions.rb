c40c81d37fb5a93da828b106b603534eb8f332c2
Khaled AbuShqear <qmax93@gmail.com>
2022-11-29 02:07:23 +0300

    183702455: Add `content` column

--- db/migrate/20221125090838_create_revisions.rb
+++ db/migrate/20221125090838_create_revisions.rb
@@ -2,12 +2,13 @@ class CreateRevisions < ActiveRecord::Migration[7.0]
   def change
     create_table :revisions do |t|
       t.belongs_to :case_report, foreign_key: true
+      t.integer :user_id
       t.column :report_type, :smallint
       t.string :responder_name
       t.string :patient_name
       t.date :patient_dob
       t.jsonb :incident_address
-      t.integer :user_id
+      t.jsonb :content
 
       t.datetime :incident_at
     end
