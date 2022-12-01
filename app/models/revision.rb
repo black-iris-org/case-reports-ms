80fe7be5d43e8ed46798c26775731016fcb336b2
Khaled AbuShqear <qmax93@gmail.com>
2022-12-01 19:45:58 +0300

    183801547: Update defaults for revisions

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -1,4 +1,10 @@
 class Revision < ApplicationRecord
   belongs_to :case_report, optional: true
   has_many :audits
+
+  before_create :set_time_stamps
+
+  def set_time_stamps
+    self.incident_at = DateTime.now
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_011812) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_163622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,9 +34,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_011812) do
     t.string "responder_name"
     t.string "patient_name"
     t.date "patient_dob"
-    t.jsonb "incident_address"
-    t.jsonb "content"
-    t.datetime "incident_at"
+    t.jsonb "incident_address", default: "{}"
+    t.jsonb "content", default: "{}"
+    t.datetime "incident_at", default: -> { "CURRENT_TIMESTAMP" }
     t.index ["case_report_id"], name: "index_revisions_on_case_report_id"
   end
 
