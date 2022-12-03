cd41ae1ba7a2d3fa967dc6edc0fcababd6f8b96e
Khaled AbuShqear <qmax93@gmail.com>
2022-12-04 00:37:36 +0300

    183801547: Set defaults for revision model

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -2,4 +2,10 @@ class Revision < ApplicationRecord
   belongs_to :case_report, optional: true
   has_many :audits
 
+  before_create :set_defaults
+
+  def set_defaults
+    self.incident_address ||= {}
+    self.content ||= {}
+  end
 end
