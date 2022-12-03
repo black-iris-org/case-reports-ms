f08267e2bd0f8ea7a1a06bf2f1638d4aa9b7e5e9
Khaled AbuShqear <qmax93@gmail.com>
2022-12-04 00:39:30 +0300

    183801547: Add validation on revisions, user & responder is required & cannot add identical revision

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -2,10 +2,19 @@ class Revision < ApplicationRecord
   belongs_to :case_report, optional: true
   has_many :audits
 
+  validates_presence_of :user_id, :responder_name
+
   before_create :set_defaults
+  validate :validate_not_identical
 
   def set_defaults
     self.incident_address ||= {}
     self.content ||= {}
+  end
+
+  def validate_not_identical
+    if case_report&.revision&.attributes&.except('id') == attributes&.except('id')
+      errors.add(:base, :cannot_be_identical)
+    end
   end
 end
