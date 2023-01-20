a73a78e60b5a0549e3aca29fd5a6fbd3ff72f613
Khaled AbuShqear <qmax93@gmail.com>
2023-01-21 02:51:43 +0300

    184005911: Revert dockerfile work

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-source /etc/profile.d/rvm.sh
+# belongs to the attempt of creating truffleruby image but it was too slow
+#source /etc/profile.d/rvm.sh
 
 if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
